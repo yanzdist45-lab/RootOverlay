edb32cfa6f87d62d9f38c555a2c8d0912655d769
yanzdist45-lab <yanzdist45@gmail.com>
2026-09-15 22:18:58 +0700

    Fix Java and Kotlin JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android {
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     namespace = "biz.shikuro.rootoverlay"
     compileSdk = 35
     defaultConfig { applicationId = "biz.shikuro.rootoverlay"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0" }
